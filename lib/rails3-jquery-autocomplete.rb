6f52cac3e0ef3732046b660b78b2408dda7f5559
Lucas Mundim <lucas.mundim@corp.globo.com>
2010-09-14 15:53:20 -0300

    Added support to MongoDB using Mongoid

--- lib/rails3-jquery-autocomplete.rb
+++ lib/rails3-jquery-autocomplete.rb
@@ -6,33 +6,40 @@ module Rails3JQueryAutocomplete
   end
 
   # Inspired on DHH's autocomplete plugin
-  # 
+  #
   # Usage:
-  # 
+  #
   # class ProductsController < Admin::BaseController
   #   autocomplete :brand, :name
   # end
   #
-  # This will magically generate an action autocomplete_brand_name, so, 
+  # This will magically generate an action autocomplete_brand_name, so,
   # don't forget to add it on your routes file
-  # 
+  #
   #   resources :products do
   #      get :autocomplete_brand_name, :on => :collection
   #   end
   #
   # Now, on your view, all you have to do is have a text field like:
-  # 
+  #
   #   f.text_field :brand_name, :autocomplete => autocomplete_brand_name_products_path
   #
   #
   module ClassMethods
     def autocomplete(object, method, options = {})
+      isMongoId = object.to_s.camelize.constantize.included_modules.collect(&:to_s).include?("Mongoid::Document")
       limit = options[:limit] || 10
-      order = options[:order] || "#{method} ASC"
+      order = options[:order]
+      order = isMongoId ? method.to_sym.asc : "#{method} ASC"
 
       define_method("autocomplete_#{object}_#{method}") do
         unless params[:term] && params[:term].empty?
-          items = object.to_s.camelize.constantize.where(["LOWER(#{method}) LIKE ?", "#{(options[:full] ? '%' : '')}#{params[:term].downcase}%"]).limit(limit).order(order)
+          if isMongoId
+            search = (options[:full] ? '.*' : '^') + params[:term] + '.*'
+            items = object.to_s.camelize.constantize.where(method.to_sym => /#{search}/i).limit(limit).order_by(method.to_sym.asc)
+          else
+            items = object.to_s.camelize.constantize.where(["LOWER(#{method}) LIKE ?", "#{(options[:full] ? '%' : '')}#{params[:term].downcase}%"]).limit(limit).order(order)
+          end
         else
           items = {}
         end
